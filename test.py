e5173c7966f12c2638ccfb30aa3581d8199508bb
lalaterry <tlf20918@gmail.com>
2020-08-05 11:17:11 +0800

    test

---
+++ test.py
@@ -0,0 +1,17 @@
+import requests
+
+def lineNotifyMessage(token, msg):
+     headers = {
+         "Authorization": "Bearer " + token, 
+         "Content-Type" : "application/x-www-form-urlencoded"
+     }
+
+     payload = {'message': msg}
+     r = requests.post("https://notify-api.line.me/api/notify", headers = headers, params = payload)
+     return r.status_code
+	
+message = 'Notify from LINE, HELLO WORLD'
+
+token = 'you token'
+
+lineNotifyMessage(token, message)
